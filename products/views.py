1b69324f7048378303909e6aace6768049476732
suzybee1987 <suzannemiller1987@gmail.com>
2021-11-30 11:45:34 +0000

    Fix: product redirect bug

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
