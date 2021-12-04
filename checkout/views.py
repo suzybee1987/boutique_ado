c9edd3ad7e92dbed177132d79bc95981a1fb1bf9
suzybee1987 <suzannemiller1987@gmail.com>
2021-12-04 09:14:07 +0000

    Dev: connect stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -4,10 +4,13 @@ from django.conf import settings
 
 from .forms import OrderForm
 from bag.contexts import bag_contents
+
 import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -16,13 +19,23 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51K2tNXB6E83KPDM03iQHRLcYR0xWPMwfk0XuvlkzXBlMPXBSRROOeRnCsGP7NsrHdtFEV8k6JWKprERnYTV8AJ6z00468A6naZ',
-        'client_secret': 'test client secret',
+        'stripe_public_key': 'stripe_public_key',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
