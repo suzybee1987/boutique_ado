f89cee125b010e1db85740f79bf8b39a93177e83
suzybee1987 <suzannemiller1987@gmail.com>
2021-12-04 09:54:52 +0000

    Bug: duplicate orders

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,16 @@
-let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-let client_secret = $('#id_client_secret').text().slice(1, -1);
-let stripe = Stripe(stripe_public_key);
-let elements = stripe.elements();
-let style = {
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
     base: {
         color: '#000',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
@@ -17,19 +25,20 @@ let style = {
         iconColor: '#dc3545'
     }
 };
-let card = elements.create('card', {
+var card = elements.create('card', {
     style: style
 });
 card.mount('#card-element');
 
+// Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
-    let errorDiv = document.getElementById('card-errors');
+    var errorDiv = document.getElementById('card-errors');
     if (event.error) {
-        let html = `
-        <span class="icon" role="alert">
-        <i class="fas fa-times"></i>
-        </span>
-        <span>${event.error.message}</span>
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
         `;
         $(errorDiv).html(html);
     } else {
@@ -37,9 +46,8 @@ card.addEventListener('change', function (event) {
     }
 });
 
-// Handle form submit 
-
-let form = document.getElementById("payment-form");
+// Handle form submit
+var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function (ev) {
     ev.preventDefault();
@@ -53,21 +61,21 @@ form.addEventListener('submit', function (ev) {
         }
     }).then(function (result) {
         if (result.error) {
-            let errorDiv = document.getElementById('card-errors');
-            let html = `
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
                 <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
                 </span>
-                <span>${result.error.message}</span>
-                `;
+                <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
             card.update({
                 'disabled': false
             });
+            $('#submit-button').attr('disabled', false);
         } else {
             if (result.paymentIntent.status === 'succeeded') {
                 form.submit();
             }
         }
-    })
-})
+    });
+});
