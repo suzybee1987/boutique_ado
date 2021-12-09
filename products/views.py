b81db607bd99ef048a64977500c83ee99fdf1389
suzybee1987 <suzannemiller1987@gmail.com>
2021-12-09 13:12:53 +0000

    Dev: create add products page

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 from django.db.models.functions import Lower
+from .forms import ProductForm
 # Create your views here.
 
 def all_products(request):
@@ -63,3 +64,14 @@ def product_detail(request, product_id):
         'product': product,
     }
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
